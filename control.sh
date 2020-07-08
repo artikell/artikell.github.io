d7d0922f11fd4a9072f2d5c5a4d37a654c100069
skyfire <739609084@qq.com>
2020-07-08 22:35:06 +0800

    2020-07-08 22:35:06 comment

--- control.sh
+++ control.sh
@@ -1,9 +1,16 @@
-git add .
-git commit -m "$(date "+%Y-%m-%d %H:%M:%S") comment"
-git push
+case $1 in
+    "commit")
+        git add .
+        git commit -m "$(date "+%Y-%m-%d %H:%M:%S") comment"
+        git push
 
-echo "git push success"
+        echo "git push success"
 
-hexo clean && hexo g && hexo d
+        hexo clean && hexo g && hexo d
 
-echo "hexo deploy success"
+        echo "hexo deploy success"
+        ;;
+    "server")
+        hexo g && hexo s
+        ;;
+esac
